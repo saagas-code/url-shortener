d5989d937043c88c293cd1c565af09f3bc714166
Matheus Almeida <saagas.code@gmail.com>
2023-09-08 11:24:51 -0300

    added config file

---
+++ back-end/src/config.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config: any = {
+    port: process.env.PORT,
+    db_port: process.env.POSTGRES_PORT,
+    db_name: process.env.POSTGRES_DB,
+    db_user: process.env.POSTGRES_USER,
+    db_password: process.env.POSTGRES_PASSWORD,
+};
+
+export default config;
