5e6390dad69b74849fcb7952de413a61cff6037b
Matheus Almeida <saagas.code@gmail.com>
2023-09-08 11:27:17 -0300

    added sequelize models

---
+++ back-end/src/database/models/Links.ts
@@ -0,0 +1,25 @@
+
+import {DataTypes} from "sequelize"
+import { sequelize } from "../pgClient"
+import crypto from "crypto"
+
+const Link = sequelize.define("links", {
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        unique: true,
+        defaultValue: crypto.randomUUID()
+    },
+    url: {
+        type: DataTypes.STRING,
+    },
+    shortener: {
+        type: DataTypes.STRING,
+        defaultValue: 0
+    },
+    clicks: {
+        type: DataTypes.INTEGER
+    }
+})
+
+export default Link
