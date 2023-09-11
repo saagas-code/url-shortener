37fc89a1dc96720b7120436c2cb205d6d71b96d4
Matheus Almeida <saagas.code@gmail.com>
2023-09-11 10:57:40 -0300

    created header component

---
+++ front-end/src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+const Header = () => {
+    return (
+        <header className="header-container">
+            <div className="header-title gradient">Shortener</div>
+            <div className=""></div>
+        </header>
+    );
+};
+
+export default Header;
