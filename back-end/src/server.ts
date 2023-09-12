949e5e051f14e8325fd9bf5407d122a22507c70b
Matheus Almeida <saagas.code@gmail.com>
2023-09-12 13:16:15 -0300

    added cors to back-end

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./routes";
 import config from "./config";
 import dotenv from "dotenv";
+import cors from "cors"
 import { sequelize } from "./database/pgClient";
 
 dotenv.config();
@@ -10,6 +11,7 @@ const port = config.port;
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 app.use(router);
 
 app.listen(port, async () => {
