1c888e3dd88b60e0d0e12c96e4a3e71c4bacc248
Matheus Almeida <saagas.code@gmail.com>
2023-09-12 16:15:42 -0300

    created redirect page

---
+++ front-end/src/pages/redirect/index.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../api/links";
+import navigateExternalRoute from "../../helpers/navigateExternalRoute";
+
+const Redirect = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    const { link } = useParams();
+
+    const fetchUrl = async () => {
+        const response = await api.getFullUrl(link!);
+        console.log(response);
+        navigateExternalRoute(response.result);
+    };
+
+    useEffect(() => {
+        fetchUrl();
+        setIsLoading(false);
+    }, []);
+
+    if (isLoading) {
+        return <div className="">Loading</div>;
+    }
+
+    return <div className="">Foi</div>;
+};
+
+export default Redirect;
