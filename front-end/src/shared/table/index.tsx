278e3a9cb41e3e48f94a72987874ddd22b720c5a
Matheus Almeida <saagas.code@gmail.com>
2023-09-13 13:40:10 -0300

    added accordion to mobile screens

--- front-end/src/shared/table/index.tsx
+++ front-end/src/shared/table/index.tsx
@@ -4,13 +4,13 @@ import { Link } from "../../api/links";
 import { UpperFirstLetter } from "../../helpers/upperFirstLetter";
 import config from "../../config";
 import copyText from "../../helpers/copyText";
+import Accordion from "./accordion";
 
 interface props {
     links: Link[];
 }
 
 const Table = ({ links }: props) => {
-
     if (links.length) {
         return (
             <div className="table-container">
@@ -18,59 +18,68 @@ const Table = ({ links }: props) => {
                     <div className="head-cell short-link">Shorten Links</div>
                     <div className="head-cell long-link">Original Link</div>
                     <div className="head-cell clicks">Clicks</div>
-                    <div className="head-cell clicks-mobile">Clicks</div>
                     <div className="head-cell qr-code">QR Code</div>
                     <div className="head-cell status">Status</div>
                     <div className="head-cell date">Date</div>
                 </div>
                 <div className="table-body">
                     {links.map((item, index) => {
-                        const { day, month, year } = getDateLink(item.createdAt);
-                        console.log(item.createdAt)
-                        console.log(day, month, year)
+                        const date = getDateLink(
+                            item.createdAt
+                        );
+                        const {day, month, year} = date
+
                         return (
-                            <div key={index} className="table-row ">
-                                <div className="short-link">
-                                    <span>{`${config.frontEndUrl}/${item.shortener}`}</span>
-                                    <img
-                                        onClick={() =>
-                                            copyText(
-                                                `${config.frontEndUrl}/${item.shortener}`
-                                            )
-                                        }
-                                        className="copy"
-                                        src="/assets/copy.svg"
-                                        alt=""
-                                    />
+                            <>
+                                <div
+                                    style={{ position: "relative" }}
+                                    className=""
+                                >
+                                    <div key={index} className="table-row ">
+                                        <div className="short-link">
+                                            <span>{`${config.frontEndUrl}/${item.shortener}`}</span>
+                                            <img
+                                                onClick={() =>
+                                                    copyText(
+                                                        `${config.frontEndUrl}/${item.shortener}`
+                                                    )
+                                                }
+                                                className="copy"
+                                                src="/assets/copy.svg"
+                                                alt=""
+                                            />
+                                        </div>
+                                        <div className="long-link">
+                                            <span>{item.url}</span>
+                                        </div>
+                                        <div className="head-cell clicks">
+                                            {item.clicks}
+                                        </div>
+                                        <div className="qr-code">
+                                            <img
+                                                src="/assets/qrcode.svg"
+                                                alt=""
+                                            />
+                                        </div>
+                                        <div className="head-cell status">
+                                            <span
+                                                className={`status-text ${item.status}`}
+                                            >
+                                                {UpperFirstLetter(item.status)}
+                                            </span>
+                                            <img
+                                                className={`status-icon ${item.status}`}
+                                                src={`/assets/link-${item.status}.svg`}
+                                                alt="status icon"
+                                            />
+                                        </div>
+                                        <div className="date">
+                                            {month.slice(0, 3)} - {day} - {year}
+                                        </div>
+                                    </div>
+                                    <Accordion link={item} linkDate={date} />
                                 </div>
-                                <div className="long-link">
-                                    <span>{item.url}</span>
-                                </div>
-                                <div className="clicks-mobile">
-                                    {item.clicks}
-                                </div>
-                                <div className="head-cell clicks">
-                                    {item.clicks}
-                                </div>
-                                <div className="qr-code">
-                                    <img src="/assets/qrcode.svg" alt="" />
-                                </div>
-                                <div className="head-cell status">
-                                    <span
-                                        className={`status-text ${item.status}`}
-                                    >
-                                        {UpperFirstLetter(item.status)}
-                                    </span>
-                                    <img
-                                        className={`status-icon ${item.status}`}
-                                        src={`/assets/link-${item.status}.svg`}
-                                        alt="status icon"
-                                    />
-                                </div>
-                                <div className="date">
-                                    {month.slice(0, 3)} - {day} - {year}
-                                </div>
-                            </div>
+                            </>
                         );
                     })}
                 </div>
