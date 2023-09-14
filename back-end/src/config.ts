5462f321e821582787741a74cb727a74cb2aeadc
Matheus Almeida <saagas.code@gmail.com>
2023-09-14 16:47:17 -0300

    removed the db_port variable from .env

--- back-end/src/config.ts
+++ back-end/src/config.ts
@@ -4,7 +4,6 @@ dotenv.config();
 
 const config: any = {
   port: process.env.PORT,
-  db_port: process.env.POSTGRES_PORT,
   db_name: process.env.POSTGRES_DB,
   db_user: process.env.POSTGRES_USER,
   db_password: process.env.POSTGRES_PASSWORD,
--- back-end/src/database/pgClient.ts
+++ back-end/src/database/pgClient.ts
@@ -1,5 +1,4 @@
 import { Sequelize } from "sequelize";
-import config from "../config";
 
 const sequelize = new Sequelize({
   dialect: "postgres",
