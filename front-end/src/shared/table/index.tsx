8923d4d62feb50f6b97910b1f5d4b2a78e81cab8
Matheus Almeida <saagas.code@gmail.com>
2023-09-15 15:07:13 -0300

    added qrcode modal for each item in table

--- front-end/src/shared/table/index.tsx
+++ front-end/src/shared/table/index.tsx
@@ -5,71 +5,91 @@ import { UpperFirstLetter } from "../../helpers/upperFirstLetter";
 import config from "../../config";
 import copyText from "../../helpers/copyText";
 import Accordion from "./accordion";
+import Modal from "../modal";
+import QrCode from "../qrCode";
+import { useState } from "react";
 
 interface props {
   links: Link[];
 }
 
 const Table = ({ links }: props) => {
+  const [isQrOpen, setIsQrOpen] = useState(false)
+  const [QrResult, setQrResult] = useState("")
+
+  const handleQrCode = (result: string) => {
+    setQrResult(result)
+    setIsQrOpen(true)
+  }
+  
+  const closeModal = () => {
+    setIsQrOpen(false)
+  }
+  
   if (links.length) {
     return (
-      <div className="table-container">
-        <div className="table-head">
-          <div className="head-cell short-link">Shorten Links</div>
-          <div className="head-cell long-link">Original Link</div>
-          <div className="head-cell clicks">Clicks</div>
-          <div className="head-cell qr-code">QR Code</div>
-          <div className="head-cell status">Status</div>
-          <div className="head-cell date">Date</div>
-        </div>
-        <div className="table-body">
-          {links.map((item, index) => {
-            const date = getDateLink(item.createdAt);
-            const { day, month, year } = date;
+      <>
+        <div className="table-container">
+          <div className="table-head">
+            <div className="head-cell short-link">Shorten Links</div>
+            <div className="head-cell long-link">Original Link</div>
+            <div className="head-cell clicks">Clicks</div>
+            <div className="head-cell qr-code">QR Code</div>
+            <div className="head-cell status">Status</div>
+            <div className="head-cell date">Date</div>
+          </div>
+          <div className="table-body">
+            {links.map((item, index) => {
+              const date = getDateLink(item.createdAt);
+              const { day, month, year } = date;
 
-            return (
-              <>
-                <div style={{ position: "relative" }} className="">
-                  <div key={index} className="table-row ">
-                    <div className="short-link">
-                      <span>{`${config.frontEndUrl}/${item.shortener}`}</span>
-                      <img
-                        onClick={() =>
-                          copyText(`${config.frontEndUrl}/${item.shortener}`)
-                        }
-                        className="copy"
-                        src="/assets/copy.svg"
-                        alt=""
-                      />
+              return (
+                <>
+                  <div style={{ position: "relative" }} className="">
+                    <div key={index} className="table-row ">
+                      <div className="short-link">
+                        <span>{`${config.frontEndUrl}/${item.shortener}`}</span>
+                        <img
+                          onClick={() =>
+                            copyText(`${config.frontEndUrl}/${item.shortener}`)
+                          }
+                          className="copy"
+                          src="/assets/copy.svg"
+                          alt=""
+                        />
+                      </div>
+                      <div className="long-link">
+                        <span>{item.url}</span>
+                      </div>
+                      <div className="head-cell clicks">{item.clicks}</div>
+                      <div onClick={() => handleQrCode(item.shortener)} className="qr-code">
+                        <img src="/assets/qrcode.svg" alt="" />
+                      </div>
+                      <div className="head-cell status">
+                        <span className={`status-text ${item.status}`}>
+                          {UpperFirstLetter(item.status)}
+                        </span>
+                        <img
+                          className={`status-icon ${item.status}`}
+                          src={`/assets/link-${item.status}.svg`}
+                          alt="status icon"
+                        />
+                      </div>
+                      <div className="date">
+                        {month.slice(0, 3)} - {day} - {year}
+                      </div>
                     </div>
-                    <div className="long-link">
-                      <span>{item.url}</span>
-                    </div>
-                    <div className="head-cell clicks">{item.clicks}</div>
-                    <div className="qr-code">
-                      <img src="/assets/qrcode.svg" alt="" />
-                    </div>
-                    <div className="head-cell status">
-                      <span className={`status-text ${item.status}`}>
-                        {UpperFirstLetter(item.status)}
-                      </span>
-                      <img
-                        className={`status-icon ${item.status}`}
-                        src={`/assets/link-${item.status}.svg`}
-                        alt="status icon"
-                      />
-                    </div>
-                    <div className="date">
-                      {month.slice(0, 3)} - {day} - {year}
-                    </div>
+                    <Accordion link={item} linkDate={date} />
                   </div>
-                  <Accordion link={item} linkDate={date} />
-                </div>
-              </>
-            );
-          })}
+                </>
+              );
+            })}
+          </div>
         </div>
-      </div>
+        <Modal isOpen={isQrOpen} onModalClose={closeModal}>
+            <QrCode link={QrResult} />
+        </Modal>
+      </>
     );
   }
 
